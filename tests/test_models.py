929b63990ea7ec097c42fa79fd5bd00908aae5ff
Richard Bann <richard.bann@vertis.com>
2015-04-30 17:13:22 +0200

    pre-initial

---
+++ tests/test_models.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_dj-timetravel-postgres
+------------
+
+Tests for `dj-timetravel-postgres` models module.
+"""
+
+import os
+import shutil
+import unittest
+
+from django_timetravel_pg import models
+
+
+class TestDj_timetravel_postgres(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
