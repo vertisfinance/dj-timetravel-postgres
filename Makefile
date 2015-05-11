b279cf8766f7aa8b152423076925ce69d8fdd445
Richard Bann <richard.bann@vertis.com>
2015-05-11 16:39:30 +0200

    coverage --combine
    No need to delete imported modules, we can run multiple
    test processes.

--- Makefile
+++ Makefile
@@ -1,5 +1,17 @@
 coverage:
-	coverage run --source dj_timetravel_postgres runtests.py
+	coverage erase
+	python commands.py remove_container djtt
+	python commands.py run_container djtt
+	python commands.py remove_migrations tests/project1
+	coverage run -p --branch --source dj_timetravel_postgres commands.py run_tests tests/project1
+
+	python commands.py remove_container djtt
+	python commands.py run_container djtt
+	python commands.py remove_migrations tests/project2
+	coverage run -p --branch --source dj_timetravel_postgres commands.py run_tests tests/project2
+
+	python commands.py remove_container djtt
+	coverage combine
 	coverage report -m
 	coverage html
 	chromium-browser htmlcov/index.html
---
+++ commands.py
@@ -0,0 +1,93 @@
+import subprocess
+import time
+import os
+import sys
+
+import click
+import django
+from django.test.runner import DiscoverRunner
+from django.core.management import call_command
+
+
+class NoDBRunner(DiscoverRunner):
+    def setup_databases(self, **kwargs):
+        pass
+
+    def teardown_databases(self, old_config, **kwargs):
+        pass
+
+
+@click.group()
+def run():
+    pass
+
+
+@run.command()
+@click.argument('container_name')
+def remove_container(container_name):
+    with open('/dev/null', 'w') as devnull:
+        try:
+            subprocess.check_call(['docker', 'stop', container_name],
+                                  stdout=devnull, stderr=devnull)
+        except subprocess.CalledProcessError:
+            pass
+
+        try:
+            subprocess.check_call(['docker', 'rm', container_name],
+                                  stdout=devnull, stderr=devnull)
+        except subprocess.CalledProcessError:
+            pass
+
+
+@run.command()
+@click.argument('container_name')
+def run_container(container_name):
+    with open('/dev/null', 'w') as devnull:
+        subprocess.check_call(['docker', 'run', '-d', '--name', container_name,
+                               '-p', '55432:5432',
+                               'vertisfinance/dj-timetravel-postgres'],
+                              stdout=devnull, stderr=devnull)
+    time.sleep(1)
+
+
+@run.command()
+@click.argument('path', type=click.Path(exists=True, file_okay=False))
+def remove_migrations(path):
+    for app in os.listdir(path):
+        full = os.path.join(path, app)
+        if os.path.isdir(full):
+            mig = os.path.join(full, 'migrations')
+            if os.path.isdir(mig):
+                for migname in os.listdir(mig):
+                    if migname != '__init__.py':
+                        migfile = os.path.join(mig, migname)
+                        os.remove(migfile)
+
+
+@run.command()
+@click.argument('path', type=click.Path(exists=True, file_okay=False))
+@click.option('--no_makemigrations', is_flag=True)
+@click.option('--no_migrate', is_flag=True)
+def run_tests(path, no_makemigrations, no_migrate):
+    orig_path = sys.path[:]
+    sys.path = [path] + orig_path
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'core.settings'
+    django.setup()
+
+    if no_makemigrations:
+        pass
+    else:
+        call_command('makemigrations', interactive=False, verbosity=0)
+
+    if no_migrate:
+        pass
+    else:
+        call_command('migrate', interactive=False, verbosity=0)
+
+    test_runner = NoDBRunner()
+    num_failures = test_runner.run_tests(['projecttests'])
+
+    sys.exit(bool(num_failures))
+
+if __name__ == '__main__':
+    run()
